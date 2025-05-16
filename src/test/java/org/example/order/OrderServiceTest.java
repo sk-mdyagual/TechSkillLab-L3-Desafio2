f5c17fd171f77b03e250c468205f88f71c37896a
Cristian Bambague <cbmelend@bancolombia.com.co>
2025-05-16 01:23:55 -0500

    feat(D2): challege two complete

---
+++ src/test/java/org/example/order/OrderServiceTest.java
@@ -0,0 +1,20 @@
+package org.example.order;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+
+    @Test
+    void processOrder() {
+    }
+
+    @Test
+    void retrieveOrder() {
+    }
+
+    @Test
+    void updateOrder() {
+    }
+}
